486b09642d7ca35fbc52e0832fc3f2134b3dd6d5
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-03-23 21:31:54 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
+import { StaticsTitle } from './App.styled'
+class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+    onLeaveFeedback = evt => {
+        const { name } = evt.target;
+        // this.setState({ [name]: this.state[name] + 1 });
+        this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    };
+    countTotalFeedback() {
+        const { good, bad, neutral } = this.state;
+        return good + bad + neutral;
+    }
+    countPositiveFeedbackPercentage() {
+        const { good, bad, neutral } = this.state;
+        const procentPositive = (good / (good + bad + neutral)) * 100;
+        return Math.round(procentPositive) || 0;
+    }
+
+    render() {
+        const { good, neutral, bad } = this.state;
+
+        return (
+            <>
+                <Section title="Please leave feedback">
+                    <FeedbackOptions
+                        options={Object.keys(this.state)}
+                        onLeaveFeedback={this.onLeaveFeedback}
+                    />
+                    <StaticsTitle>Statictics</StaticsTitle>
+                    {this.countTotalFeedback() > 0 ? (
+                        <Statistics
+                            good={good}
+                            neutral={neutral}
+                            bad={bad}
+                            total={this.countTotalFeedback()}
+                            positivePercentage={this.countPositiveFeedbackPercentage()}
+                        />
+                    ) : (
+                        <Notification message="There is no feedback" />
+                    )}
+                </Section>
+            </>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StaticsTitle = styled.h2`
+    font-size: 30px;
+    margin-bottom: 20px;
+    color: #FFF0F5
+`;
